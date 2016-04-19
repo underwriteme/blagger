b7a75818d53c837f07aac3bf5e6ce52bf3f10fea
Chris Prior <chris.prior@underwriteme.co.uk>
2016-04-20 07:30:05 +1000

    Reformat test

--- test/unit/blagger/BlogControllerSpec.groovy
+++ test/unit/blagger/BlogControllerSpec.groovy
@@ -1,7 +1,6 @@
 package blagger
 
 import grails.test.mixin.*
-import org.junit.*
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
@@ -10,52 +9,48 @@ import org.junit.*
 @Mock(Post)
 class BlogControllerSpec extends spock.lang.Specification {
 
-    def 'List returns the blag posts'(){
-
-        setup:
-            new Post(title: 'test', email: 'test@test.com', content: 'test').save()
-
-        and:
-            def model = controller.list()
-
-        expect:
-            model.posts
-            model.posts[0].title == 'test'
-    }
-
-
-    def 'List returns the blag posts in the correct order'(){
-
-        setup:
-            new Post(title: 'test1', email: 'test@test.com', content: 'test').save()
-            new Post(title: 'test2', email: 'test@test.com', content: 'test').save()
-
-        and:
-            def model = controller.list()
-
-        expect:
-            model.posts
-            model.posts[0].title == 'test2'
-    }
-
-
-    def 'Create adds a new blag post and redirects'() {
-
-        setup:
-            controller.params.title = 'test'
-            controller.params.email = 'test@test.com'
-            controller.params.content = 'test'
+    def 'list returns the blag posts'(){
+        given:
+        new Post(title: 'test', email: 'test@test.com', content: 'test').save()
 
         when:
-            controller.createPost()
-            def posts = Post.list()
+        def model = controller.list()
 
         then:
-            posts
-            posts[0].title == 'test'
-            posts[0].email == 'test@test.com'
-            posts[0].content == 'test'
-            response.redirectUrl.endsWith('/list')
+        model.posts
+        model.posts[0].title == 'test'
+    }
+
+    def 'list returns the blag posts in the correct order'(){
+        given:
+        new Post(title: 'test1', email: 'test@test.com', content: 'test').save()
+        new Post(title: 'test2', email: 'test@test.com', content: 'test').save()
+
+        when:
+        def model = controller.list()
+
+        then:
+        model.posts
+        model.posts[0].title == 'test2'
+    }
+
+
+    def 'create adds a new blag post and redirects'() {
+        given:
+        controller.params.title = 'test'
+        controller.params.email = 'test@test.com'
+        controller.params.content = 'test'
+
+        when:
+        controller.createPost()
+        def posts = Post.list()
+
+        then:
+        posts
+        posts[0].title == 'test'
+        posts[0].email == 'test@test.com'
+        posts[0].content == 'test'
+        response.redirectUrl.endsWith('/list')
     }
 
 }
