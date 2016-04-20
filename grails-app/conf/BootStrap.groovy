2b5b5f0a669b64cad94448a8517592dc7bf53bb5
Chris Prior <chris.prior@underwriteme.co.uk>
2016-04-20 11:02:48 +1000

    Update challenge message to only reference tag story

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,24 +6,6 @@ class BootStrap {
 
         def content = """\
         <dl>
-            <dt>Validate inputs for a new blag post</dt>
-            <dd>
-                <ol>
-                    <li>For a new blag post, ensure the following are true</li>
-                        <ol>
-                            <li>There is a title of at least 3 characters</li>
-                            <li>There is a valid email address</li>
-                            <li>There is some content</li>
-                        </ol>
-                    <li>Display any relevant errors alongside the form controls</li>
-                </ol>
-            </dd>
-            <dt>Paginate blag posts</dt>
-            <dd>
-                <ol>
-                    <li>On the home page, paginate the blag posts, 5 at a time</li>
-                </ol>
-            </dd>
             <dt>Add the ability to create tags on new blag posts</dt>
             <dd>
                 <ol>
@@ -32,7 +14,6 @@ class BootStrap {
                     <li>Allow the filtering of blag posts by tag on the home page</li>
                 </ol>
             </dd>
-            <dt>Create and email a patch with your changes as specified in Git<dt>
         </dl>
         """
 
