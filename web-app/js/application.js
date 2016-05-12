533d6ceaa488cbd698e5170ab6fb95b6a897ecaf
Chris Prior <chris.prior@underwriteme.co.uk>
2016-05-12 17:05:56 +1000

    Remove typeahead implementation

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -1,17 +1,1 @@
 
-$(function() {
-    $("#tag").autocomplete({
-        source: function(request, response) {
-            $.ajax({
-                url: "/blagger/tag/list",
-                data: {
-                    stem: request.term
-                },
-                success: function(data) {
-                    response(data);
-                }
-            });
-        },
-        minLength: 3,
-    });
-});
