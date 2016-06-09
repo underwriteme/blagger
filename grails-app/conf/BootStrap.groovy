1f1e55bd08ce97fcb2cb9accd8dbb0fecd731e41
chris-prior <chris.m.prior@gmail.com>
2016-06-09 15:42:01 +0100

    Make instructions clearer

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -9,7 +9,7 @@ class BootStrap {
             <dt>Add the ability to create tags on new blag posts</dt>
             <dd>
                 <ol>
-                    <li>Add the ability to tag a new blag post with a (single) category</li>
+                    <li>Add the ability to add a single tag to a new blag post. (For instance, tagging a particular post that it relates to "programming")</li>
                     <li>Use a typeahead control to suggest existing tags once at least 3 characters have been entered</li>
                     <li>Allow the filtering of blag posts by tag on the home page</li>
                 </ol>
